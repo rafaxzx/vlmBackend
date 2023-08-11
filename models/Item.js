fa7f8d54f5d2d65bcb789d210ec28ea4d7aed695
Rafael <rafa_xzx@hotmail.com>
2023-08-11 18:17:53 -0300

    Created model for table items

--- models/Item.js
+++ models/Item.js
@@ -1,4 +1,18 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/sequelize");
 
-const Item = require("items", {});
+const Item = sequelize.define("items", {
+  codeIntern: DataTypes.INTEGER,
+  name: DataTypes.STRING,
+  specification: DataTypes.STRING,
+  equipSectorId: DataTypes.INTEGER,
+  manufacturersId: DataTypes.INTEGER,
+  trayNumber: DataTypes.INTEGER,
+  trayLocation: DataTypes.STRING,
+  minimumStock: DataTypes.INTEGER,
+  maxStock: DataTypes.INTEGER,
+  currentStock: DataTypes.INTEGER,
+  image: DataTypes.BLOB,
+});
+
+module.exports = Item;
