2944a861904b85553617007680a605c6add81f97
Rafael <rafa_xzx@hotmail.com>
2023-08-11 12:36:18 -0300

    Creation of migration for table manufacturers

---
+++ migrations/20230811152046-manufacturers.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("manufacturers", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      logo: {
+        type: Sequelize.BLOB,
+        allowNull: true,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("manufacturers");
+  },
+};
