89923a741e5f59234db3ea8158f5d4920e0bf0d5
Rafael <rafa_xzx@hotmail.com>
2023-08-15 12:47:38 -0300

    Created file for routes in directory SRC

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const routes = express.Router();
+
+//Import of controllers
+const ManufacturerController = require("../controllers/ManufacturerController");
+const EquipSectorController = require("../controllers/EquipSectorController");
+
+//Routes for table manufacturers C.R.U.D.
+routes.post("/manufacturers", ManufacturerController.store);
+routes.get("/manufacturers", ManufacturerController.findAll);
+routes.put("/manufacturers/:id", ManufacturerController.put);
+routes.delete("/manufacturer/:id", ManufacturerController.destroy);
+
+//Routes for table equipSectors C.R.U.D.
+routes.post("/equipSectors", EquipSectorController.store);
+routes.get("/equipSectors", EquipSectorController.findAll);
+routes.putt("/equipSectors/:id", EquipSectorController.put);
+routes.delete("/equipSectors/:id", EquipSectorController.destroy);
+
+module.exports = routes;
