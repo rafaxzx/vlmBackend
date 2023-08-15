f34f33deb4a7b3cedaa20d87e6b2693c09aecd26
Rafael <rafa_xzx@hotmail.com>
2023-08-15 12:11:35 -0300

    Created controller to EquipSector table

---
+++ controllers/EquipSectorController.js
@@ -0,0 +1,35 @@
+const EquipSector = require("../models/EquipSector");
+
+module.exports = {
+  //Create an EquipSector
+  async store(req, res) {
+    const { equipSector } = req.body;
+    const newEsquipSector = await EquipSector.create({ equipSector });
+    return res.json(newEsquipSector);
+  },
+  //FindAll
+  async findAll(req, res) {
+    const allSectors = await EquipSector.findAll();
+    return res.json(allSectors);
+  },
+  //Find one
+  async findOne(req, res) {
+    const oneSector = await EquipSector.findAll({
+      where: { equipSector: req.params.equipSector },
+    });
+  },
+  //Update one specific equipSector
+  async put(req, res) {
+    const { equipSector } = req.body;
+    await EquipSector.update(
+      { equipSector },
+      { where: { id: req.params.equipSector } }
+    );
+    return res.send("Update with success.");
+  },
+  //Delete an equipSector
+  async destroy(req, res) {
+    await EquipSector.destroy({ where: { id: req.params.id } });
+    return res.send("Equipament and/or Sector deleted with success.");
+  },
+};
