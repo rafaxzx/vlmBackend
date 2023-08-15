c56e1764767e5c2cbd3421d7478427ac38f10f9b
Rafael <rafa_xzx@hotmail.com>
2023-08-15 20:43:15 -0300

    Updated the routes.js with the items end points

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const routes = express.Router();
 //Import of controllers
 const ManufacturerController = require("../controllers/ManufacturerController");
 const EquipSectorController = require("../controllers/EquipSectorController");
+const ItemController = require("../controllers/ItemController");
 
 //Routes for table manufacturers C.R.U.D.
 routes.post("/manufacturers", ManufacturerController.store);
@@ -17,5 +18,11 @@ routes.post("/equipSectors", EquipSectorController.store);
 routes.get("/equipSectors", EquipSectorController.findAll);
 routes.put("/equipSectors/:id", EquipSectorController.put);
 routes.delete("/equipSectors/:id", EquipSectorController.destroy);
+
+//Routes for table items
+routes.post("/items", ItemController.store);
+routes.get("/items", ItemController.findAll);
+routes.put("/items", ItemController.put);
+routes.delete("/items", ItemController.destroy);
 
 module.exports = routes;
